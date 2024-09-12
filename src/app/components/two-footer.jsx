e68f6a84c5c69f358cfed9ce48149999368dcd42
Shaun Menezes <shaunshanil95@gmail.com>
2024-09-12 19:08:29 +0800

    fix footer for mobile

--- src/app/components/two-footer.jsx
+++ src/app/components/two-footer.jsx
@@ -1,22 +1,24 @@
 export default function TwoFooter() {
   return (
-    <footer className="bg-black text-white py-6 px-4 md:px-6 flex justify-between items-center">
-      <div className="text-xl sm:text-xl md:text-2xl lg:text-3xl font-bold tracking-tighter">
-        <span className="text-blue-500">Hey</span>
-        <span className="text-white">Learn</span>
-        <span className="text-blue-500">2</span>
-        <span className="text-white">Code</span>
-      </div>
-      <div className="flex items-center space-x-4">
-        <a
-          href="mailto:shaun@heylearn2code.com"
-          className="hover:text-blue-500"
-        >
-          shaun@heylearn2code.com
-        </a>
-        <a href="https://wa.me/+60123397028" className="hover:text-blue-500">
-          +6 012 339 7028
-        </a>
+    <footer className="bg-black text-white py-6 px-4 md:px-6">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+        <div className="text-xl sm:text-xl md:text-2xl lg:text-3xl font-bold tracking-tighter">
+          <span className="text-blue-500">Hey</span>
+          <span className="text-white">Learn</span>
+          <span className="text-blue-500">2</span>
+          <span className="text-white">Code</span>
+        </div>
+        <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
+          <a
+            href="mailto:shaun@heylearn2code.com"
+            className="hover:text-blue-500"
+          >
+            shaun@heylearn2code.com
+          </a>
+          <a href="https://wa.me/+60123397028" className="hover:text-blue-500">
+            +6 012 339 7028
+          </a>
+        </div>
       </div>
     </footer>
   );
